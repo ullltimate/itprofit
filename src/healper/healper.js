97aba8404c20b83beb56bdb87ec1bf5431c085fd
ullltimate <ullltimate.zp@gmail.com>
2023-10-29 09:56:04 +0300

    feat: add send form with api

--- src/healper/healper.js
+++ src/healper/healper.js
@@ -1,5 +1,7 @@
+export const urlApi = 'http://localhost:9090';
+
 export function createWarning(elem, messageWarning){
-    elem.style.border = "1px solid red";
+    if(elem.type != 'submit') elem.style.border = "1px solid red";
     if (!elem.nextElementSibling){
         const parentInput = elem.parentNode;
         const warning = document.createElement('p');
@@ -14,4 +16,12 @@ export function removeWarning(elem){
     if (elem.nextElementSibling){
         elem.nextElementSibling.remove();
     }
+}
+
+export function checkWarning(arrayInputs){
+    return arrayInputs.every(e => !e.nextElementSibling)
+}
+
+export function clearForm(arrayInputs){
+    arrayInputs.forEach(e => e.value = '');
 }
--- src/modules/form.js
+++ src/modules/form.js
@@ -1,5 +1,7 @@
 import IMask from 'imask';
 import { validation } from './validation';
+import { checkWarning, clearForm, createWarning, removeWarning } from '../healper/healper';
+import { sendForm } from './api';
 
 export function form(){
     const inputName = document.getElementById('name');
@@ -7,6 +9,7 @@ export function form(){
     const inputPhone = document.getElementById('phone');
     const inputMessage = document.getElementById('message');
     const form = document.querySelector('.contacts-form');
+    const btnSubmit = document.querySelector('.contacts-form__content-button');
     const arrayInputs = [inputName, inputEmail, inputPhone, inputMessage];
 
     IMask(
@@ -16,9 +19,22 @@ export function form(){
         }
       )
     
-    form.addEventListener('submit', (e) => {
+    form.addEventListener('submit', async(e) => {
         e.preventDefault();
         validation(arrayInputs);
+        if(checkWarning(arrayInputs)){
+            const formData = new FormData(form);
+            const result = await sendForm(formData);
+            if(result.status === 'success'){
+                clearForm(arrayInputs);
+                btnSubmit.value = result.message;
+                btnSubmit.disabled = true;
+                setTimeout(() => {btnSubmit.value = "Submit"; btnSubmit.disabled = false}, 2000);
+                removeWarning(btnSubmit);
+            } else {
+                createWarning(btnSubmit, `${result.message}. Please try again!`);
+            }
+        }
     })
 
 }
