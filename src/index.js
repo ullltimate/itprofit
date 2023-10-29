9deb403ba1fd853588ec406f935b94c40bac2856
ullltimate <ullltimate.zp@gmail.com>
2023-10-29 20:33:37 +0300

    refactor: dinamic content loading with js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import './styles/index.scss'
 import { showModal } from './modules/modal';
 import { form } from './modules/form';
+import { content } from './modules/content';
 
 function App(){
+    const main = document.querySelector('main');
+    main.innerHTML = content;
     showModal();
     form();
 }
